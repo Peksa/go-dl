dfa23d2b5617f69ba915b9c7780172cbfbca64a7
Peksa <git@peksa.se>
2014-10-15 21:53:59 +0200

    Able to copy response body and serve

--- src/go-dl.go
+++ src/go-dl.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	dl := r.FormValue("dl")
+	if r.URL.Path != "/" || dl == "" {
+		BadRequest(w)
+		return
+	}
+
+	log.Println(dl)
+
+	resp, err := http.Get(dl)
+	if err != nil {
+		log.Fatalf("http.Get => %v", err.Error())
+	}
+
+	io.Copy(w, resp.Body)
+
+	if err != nil {
+		log.Fatalf("io.Copy => %v", err.Error())
+	}
+}
+
+func BadRequest(c http.ResponseWriter) {
+	http.Error(c, "Sad web server is sad :(", http.StatusBadRequest)
+}
 
 func main() {
-	fmt.Println("hello world")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8081", nil)
 }
