c3ef6330c49db93f27ae704b7e9f945bc21a6733
Peksa <git@peksa.se>
2014-10-16 20:58:45 +0200

    Add correct download headers. Fixes #2

--- src/go-dl.go
+++ src/go-dl.go
@@ -2,23 +2,34 @@ package main
 
 import (
 	"io"
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	dl := r.FormValue("dl")
-	if r.URL.Path != "/" || dl == "" {
+	if r.URL.Path != "/" {
+		NotFound(w);
+		return
+	}
+	dl, err := url.Parse(r.FormValue("dl"));
+	if err != nil || !dl.IsAbs() {
 		BadRequest(w)
 		return
 	}
-	log.Println("GET: " + dl)
-	resp, err := http.Get(dl)
+	log.Println("GET: " + dl.String())
+	resp, err := http.Get(dl.String())
 	if err != nil {
 		log.Println("ERROR: http.Get => %v", err.Error())
 		InternalServerError(w, "Failure fetching URL. Wrong protocol? DNS correct? Connection refused?")
 		return
 	}
+	tokens := strings.Split(dl.Path, "/")
+	fileName := tokens[len(tokens)-1];
+	w.Header().Add("Content-Type", "application/octet-stream");
+	w.Header().Add("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", fileName))
 	_, err = io.Copy(w, resp.Body)
 
 	if err != nil {
@@ -29,6 +40,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func BadRequest(w http.ResponseWriter) {
 	http.Error(w, "Sad web server is sad :(", http.StatusBadRequest)
+}
+
+func NotFound(w http.ResponseWriter) {
+	http.Error(w, "404 nothing here", http.StatusNotFound)
 }
 
 func InternalServerError(w http.ResponseWriter, message string) {
